72209d67fe2de0db5265b635a79227480b3b2e2b
wengchaoxi <s_wengchaoxi@kingsoft.com>
2022-07-08 15:33:40 +0800

    examples: Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -9,23 +9,23 @@ terraform {
 
 provider "filesystem" {
   # Or use FILESYSTEM_SECRET_KEY env var
-  secret_key = "secret_key_helloworld"
+  secret_key = "h3110_w0r1d"
 }
 
-resource "filesystem_file" "this" {
+resource "filesystem_file" "writer" {
   path    = "./helloworld.txt"
   content = "你好世界, Hello World"
 }
 
-data "filesystem_file" "this" {
-  path = filesystem_file.this.id
+data "filesystem_file" "reader" {
+  path = filesystem_file.writer.path
 }
 
 output "file_details" {
   value = <<EOH
-  name:    ${data.filesystem_file.this.filename}
-  size:    ${data.filesystem_file.this.size}
-  update:  ${data.filesystem_file.this.update_time}
-  content: ${data.filesystem_file.this.content}
+  name:    ${data.filesystem_file.reader.filename}
+  size:    ${data.filesystem_file.reader.size}
+  update:  ${data.filesystem_file.reader.update_time}
+  content: ${data.filesystem_file.reader.content}
 EOH
 }
